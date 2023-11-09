660c6bb356939aabb1efc7eb5af33ae2dc7b3273
Kassandra Cumpian <757140@student.dusd.net>
2023-11-09 15:26:06 -0800

    Added Damage
    Today I added damage and more to collectables.

--- RubysAdventureCumpian/Assets/Scripts/RubyController.cs
+++ RubysAdventureCumpian/Assets/Scripts/RubyController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health {get { return currentHealth; } }
     int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -18,7 +23,16 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer > 0)
+            {
+                isInvincible = false;
+            }
+        }
+        
     }
 
     // Update is called once per frame
@@ -27,6 +41,14 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
   
     }
     void FixedUpdate()
@@ -39,6 +61,15 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
