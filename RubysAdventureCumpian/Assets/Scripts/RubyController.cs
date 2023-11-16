65f7c13f95e7722c57c9c6a9faed8ee9be216f7b
Kassandra Cumpian <757140@student.dusd.net>
2023-11-16 15:26:59 -0800

    Added animation
    Today I animated my enemies and animated the main character

--- RubysAdventureCumpian/Assets/Scripts/RubyController.cs
+++ RubysAdventureCumpian/Assets/Scripts/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health {get { return currentHealth; } }
     int currentHealth;
 
@@ -18,11 +18,15 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 LookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
         if (isInvincible)
         {
@@ -40,6 +44,17 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            LookDirection.Set(move.x, move.y);
+            LookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", LookDirection.x);
+        animator.SetFloat("Look Y", LookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -63,6 +78,8 @@ public class RubyController : MonoBehaviour
     {
         if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
